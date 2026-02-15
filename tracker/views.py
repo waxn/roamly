2eded5658c78d18ab89a7364f11cc994523a5b13
Linus Thoreson <linusthoreson@gmail.com>
2026-02-15 18:06:44 -0500

    v0.5.1

--- tracker/views.py
+++ tracker/views.py
@@ -61,7 +61,7 @@ def reverse_geocode(lat, lon):
             f'?lat={lat}&lon={lon}&format=json&zoom=10&addressdetails=1'
         )
         headers = {
-            'User-Agent': 'Roamly/0.5.0 (self-hosted location tracker)',
+            'User-Agent': 'Roamly/0.5.1 (self-hosted location tracker)',
             'Accept-Language': 'en',
         }
         req = urllib.request.Request(url, headers=headers)
