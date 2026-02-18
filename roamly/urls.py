9014264b474506f299fc8f132aae5ed93706a8fc
Linus Thoreson <linusthoreson@gmail.com>
2026-02-18 16:35:44 -0500

    Fixed image loading

--- roamly/urls.py
+++ roamly/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,6 @@ urlpatterns = [
 ]
 
 # Serve media files (profile pictures, blurb photos) in all environments
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
