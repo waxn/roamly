73ce6c23384ae10aec7086f4cd193d883bc56056
Linus Thoreson <linusthoreson@gmail.com>
2026-02-17 19:44:43 -0500

    Add CSRF again

--- roamly/settings.py
+++ roamly/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-production')
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,roamly.waxnflaxn.xyz').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost,http://127.0.0.1,https://roamly.waxnflaxn.xyz,http://roamly.waxnflaxn.xyz').split(',')
 
 INSTALLED_APPS = [
